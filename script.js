425ecd9dd6699cac094faa812d6351366bcde902
Vincent0523 <ce-vaantepim0523@st.umat.edu.gh>
2024-11-16 14:58:39 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+
+
+
+/* ----- TYPING EFFECT ----- */
+ var typingEffect = new Typed(".typedText",{
+    strings : ["Designer","Youtuber","Developer"],
+    loop : true,
+    typeSpeed : 100, 
+    backSpeed : 80,
+    backDelay : 2000
+ })
+
+
+/* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
+ const sr = ScrollReveal({
+        origin: 'top',
+        distance: '80px',
+        duration: 2000,
+        reset: true     
+ })
+
+/* -- HOME -- */
+sr.reveal('.featured-text-card',{})
+sr.reveal('.featured-name',{delay: 100})
+sr.reveal('.featured-text-info',{delay: 200})
+sr.reveal('.featured-text-btn',{delay: 200})
+sr.reveal('.social_icons',{delay: 200})
+sr.reveal('.featured-image',{delay: 300})
+
+
+/* -- PROJECT BOX -- */
+sr.reveal('.project-box',{interval: 200})
+
+/* -- HEADINGS -- */
+sr.reveal('.top-header',{})
+
+/* ----- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION -- ## ----- */
+
+/* -- ABOUT INFO & CONTACT INFO -- */
+const srLeft = ScrollReveal({
+  origin: 'left',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+})
+
+srLeft.reveal('.about-info',{delay: 100})
+srLeft.reveal('.contact-info',{delay: 100})
+
+/* -- ABOUT SKILLS & FORM BOX -- */
+const srRight = ScrollReveal({
+  origin: 'right',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+})
+
+srRight.reveal('.skills-box',{delay: 100})
+srRight.reveal('.form-control',{delay: 100})
+
+// Get the toggle button
+const darkModeToggle = document.getElementById('darkModeToggle');
+
+// Check if dark mode is saved in localStorage
+if (localStorage.getItem('darkMode') === 'enabled') {
+    document.body.classList.add('dark-mode');
+}
+
+// Toggle dark mode on button click
+darkModeToggle.addEventListener('click', () => {
+    // Toggle the dark-mode class on the body element
+    document.body.classList.toggle('dark-mode');
+
+    // If dark mode is enabled, save it in localStorage
+    if (document.body.classList.contains('dark-mode')) {
+        localStorage.setItem('darkMode', 'enabled');
+    } else {
+        localStorage.removeItem('darkMode');
+    }
+});
